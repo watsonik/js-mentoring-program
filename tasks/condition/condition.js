13d702bbe8192e8b88959e2a8f1c614c42f83040
Sakun, Ihar <ihar.sakun@thomsonreuters.com>
2021-06-04 17:01:13 +0300

    module 3 HW Conditions

--- tasks/condition/condition.js
+++ tasks/condition/condition.js
@@ -11,9 +11,13 @@ let z2;
 const x = 3;
 const y = 7;
 
-//TODO put your code here
+if (y < 10) {
+    z1 = x + y;
+} else {
+    z1 = x * y;
+}
 
-
+z2 = (y < 10) ? (x + y) : (x * y);
 
 /** TODO
  * The system receives 3 params - alpha, beta, operator.
@@ -26,7 +30,23 @@ const alpha = 3;
 const beta = 7;
 const operator = "add";
 
-//TODO put your code here
+switch (operator) {
+    case 'add':
+        task2 = alpha + beta;
+        break;
+    case 'subtract':
+        task2 = alpha - beta;
+        break;
+    case 'multiply':
+        task2 = alpha * beta;
+        break;
+    case 'divide':
+        task2 = alpha / beta;
+        break;
+    default:
+        console.log('Unknown operator');
+        break;
+}
 
 
 
@@ -44,7 +64,10 @@ const operator = "add";
 const redemption = 420;
 let discount;
 
-//TODO put your code here
+if (redemption - 351 < 0) discount = 0;
+else if (redemption - 1351 < 0) discount = 15;
+else if (redemption - 2701 < 0) discount = 30;
+else if (redemption - 6501 < 0) discount = 45;
 
 
 module.exports = {
